1cf4c9026c7d302ae838ecf82cb70ddf33af55dc
Sarthak Jain <93116216+SarthakJain29@users.noreply.github.com>
2025-07-14 20:37:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-//your JS code here. If required.
+let year = prompt("Enter no of days", "2025");
+
+function daysOfAYear(n){
+	n = parseInt(n); 
+	if((n%4 === 0 && n%100 !== 0) || n%400 === 0){
+		return 366;
+	}else{
+		return 365;
+	}
+}
+alert(daysOfAYear(year));
